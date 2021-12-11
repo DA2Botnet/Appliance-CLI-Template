45b5ac00929b68ae9ee0767ad3643f6c8eece64d
The1TrueJoe <jttelaak@hotmail.com>
2021-12-10 22:41:02 -0500

    Rust App

---
+++ cli/target/rls/debug/deps/cli-54615cbcafbadd0d.d
@@ -0,0 +1,5 @@
+/Users/Joseph/Desktop/GitHub/Appliance-CLI-Template/cli/target/rls/debug/deps/cli-54615cbcafbadd0d.rmeta: src/main.rs
+
+/Users/Joseph/Desktop/GitHub/Appliance-CLI-Template/cli/target/rls/debug/deps/cli-54615cbcafbadd0d.d: src/main.rs
+
+src/main.rs:
---
+++ cli/target/rls/debug/deps/cli-c86a47cdb481a7d2.d
@@ -0,0 +1,5 @@
+/Users/Joseph/Desktop/GitHub/Appliance-CLI-Template/cli/target/rls/debug/deps/cli-c86a47cdb481a7d2.rmeta: src/main.rs
+
+/Users/Joseph/Desktop/GitHub/Appliance-CLI-Template/cli/target/rls/debug/deps/cli-c86a47cdb481a7d2.d: src/main.rs
+
+src/main.rs:
